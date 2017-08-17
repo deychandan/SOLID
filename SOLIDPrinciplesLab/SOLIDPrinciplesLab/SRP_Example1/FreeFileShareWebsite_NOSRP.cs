e7efb4d9034c00c81a2e74d2775e8dbc6ab617b3
Chandan Dey <deychandan@hotmail.com>
2017-08-17 15:39:13 +0530

    Added

---
+++ SOLIDPrinciplesLab/SOLIDPrinciplesLab/SRP_Example1/FreeFileShareWebsite_NOSRP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOLIDPrinciplesLab
+{
+    public class FreeFileShareWebsite_NOSRP
+    {
+        public string FileUpload(string filePath)
+        {
+            string UploadMessage = string.Empty;
+            try
+            {
+                UploadMessage = "File Uploaded Successufully";
+            }
+            catch(Exception ex)
+            {
+                System.IO.File.WriteAllText(@"C:\Project\SOLID\LogError.txt", ex.ToString());
+            }
+            return UploadMessage;
+        }
+    }
+}
---
+++ SOLIDPrinciplesLab/SOLIDPrinciplesLab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SOLIDPrinciplesLab.Startup))]
+namespace SOLIDPrinciplesLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
